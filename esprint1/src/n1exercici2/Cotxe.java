f5e29eb055c8097c0c7ee71c3af93e92f561c6a7
JosepOli <josepolive89@gmail.com>
2022-12-04 18:08:15 +0100

    Primer summit de l'exercici2

---
+++ esprint1/src/n1exercici2/Cotxe.java
@@ -0,0 +1,12 @@
+package n1exercici2;
+
+public class Cotxe {
+	
+	//Hem de fer que marca sigui static final, model static i potencia final
+	private static final String marca = " ";
+	private static String model;
+	private final Integer potencia = 120;
+	
+	
+
+}
---
+++ esprint1/src/n1exercici2/N1exercici2.java
@@ -0,0 +1,10 @@
+package n1exercici2;
+
+public class N1exercici2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
