33c6a12d4b0f755396ef4747f3087523dc8fa846
JosepOli <josepolive89@gmail.com>
2022-12-04 18:52:23 +0100

    Exercici2 complet

--- esprint1/src/n1exercici2/Cotxe.java
+++ esprint1/src/n1exercici2/Cotxe.java
@@ -2,11 +2,42 @@ package n1exercici2;
 
 public class Cotxe {
 	
-	//Hem de fer que marca sigui static final, model static i potencia final
-	private static final String marca = " ";
+	//Enunciat: Hem de fer que marca sigui static final, model static i potencia final
+	//Els "final" han de tenir valor, ja que aquest sera el seu valor sempre. (Utilitzat per a constants)
+	
+	
+	private static final String marca = "Seat";
 	private static String model;
 	private final Integer potencia = 120;
 	
 	
+	public Cotxe () {
+		Cotxe.model = "Ibiza"; //Inicialitzem atribut al constructor
+		
+	}
+	public static String getModel() {
+		return model;
+	}
 
+	public static void setModel(String model) {
+		Cotxe.model = model;
+	}
+
+	public static String getMarca() {
+		return marca;
+	}
+
+	public Integer getPotencia() {
+		return potencia;
+	}
+
+	static String frenar() {
+		return "El vehicle esta frenant.";
+	}
+	
+	String accelerar() {
+		return "El vehicle esta accelerant.";
+	}
+
+	
 }
--- esprint1/src/n1exercici2/N1exercici2.java
+++ esprint1/src/n1exercici2/N1exercici2.java
@@ -5,6 +5,33 @@ public class N1exercici2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Instanciem dos cotxes per a demostrar com funcionen els atributs estatic i
+		// final
+		Cotxe cotxe1 = new Cotxe();
+		Cotxe cotxe2 = new Cotxe();
+
+		System.out.println(
+				"Ara mateix tenim dos cotxes, com que els seus valors estan donats a la classe, seran iguals. Cotxe1:  "
+						+ cotxe1.getMarca() + " " + cotxe1.getModel() + " " + cotxe1.getPotencia() + " cv i cotxe2: "
+						+ cotxe2.getMarca() + " " + cotxe2.getModel() + " " + cotxe2.getPotencia() + " cv.");
+
+		// Seria més correcte utilitzar Cotxe en lloc de cotxe1 i cotxe2 perquè els
+		// valors son tots estàtics i per tant compartits entre els diferents objectes
+		// Podem canviar el model perquè no es final, però en ser static, canviarà per a
+		// tots els objectes
+
+		System.out.println("Canviem el valor de model. Ibiza per Leon.");
+		Cotxe.setModel("Leon");
+
+		System.out.println("Com podem veure, els dos cotxes han passat de ser Ibiza a ser Leon. Cotxe1: "
+				+ cotxe1.getModel() + " i cotxe2: " + cotxe2.getModel());
+
+		System.out.println("El correcte seria cridar els atributs estatics de la seguent manera: " + Cotxe.getMarca()
+				+ " " + Cotxe.getModel());
+
+		System.out.println("Ara cridarem els metodes accelerar i frenar. Accelerar: " + cotxe1.accelerar()
+				+ "\nFrenar: " + Cotxe.frenar());
+
 	}
 
 }
