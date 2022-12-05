632e2ea3532c4b6ef40910c4aba3fe8704221808
JosepOli <josepolive89@gmail.com>
2022-12-05 11:25:14 +0100

    TascaS1.03 Exercici3 Complet

--- TascaS103/src/n1exercici3/N1exercici3.java
+++ TascaS103/src/n1exercici3/N1exercici3.java
@@ -8,32 +8,41 @@ public class N1exercici3 {
 		// TODO Auto-generated method stub
 
 		Map<String, String> lecturaHash = new HashMap<>();
-		
+
 		Scanner sc = new Scanner(System.in);
 
 		int puntuacio = 0;
 
 		System.out.println("Introdueix el nom d'usuari.");
 		String nom = sc.nextLine();
-		
-		Random generador = new Random();
-		
+
 		lecturaHash = LectEsc.lectura();
-		
-		System.out.println(lecturaHash);
-		
-		for(int i = 0; i<10; i++) {
-			
+
+		// Busquem paisos de forma aleatoria
+		for (int i = 0; i < 10; i++) {
+			Random random = new Random();
+
+			// Passem els valors de key a una array per a poderlos agafar de manera
+			// aleatoria
+			Object[] pais = lecturaHash.keySet().toArray();
+			String randomPais = (String) pais[random.nextInt(pais.length)];
+
+			// Donem el valor de la capital associat al pais aleatori
+			String capitalPaisRandom = lecturaHash.get(randomPais);
+
+			System.out.println("Introdueix la capital del pais: " + randomPais + "\n");
+			String entrada = sc.nextLine();
+			if (entrada.equalsIgnoreCase(capitalPaisRandom))
+				puntuacio++;
+
 		}
-		
+
 		LectEsc.escriptura(nom, puntuacio);
-		
+
 	}
-	
+
 	public static boolean busquedaCapital() {
-		
-		
-		
+
 		return true;
 	}
 
