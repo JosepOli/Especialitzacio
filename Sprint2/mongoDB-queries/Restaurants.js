6d643c869d3ca4bb1069ca1ed59902487f97bb4f
JosepOli <josepolive89@gmail.com>
2023-02-21 15:29:28 +0100

    First Commit

---
+++ Sprint2/mongoDB-queries/Restaurants.js
@@ -0,0 +1,8 @@
+db.Restaurants.find()
+db.Restaurants.find({}, {restaurant_id:1, name:1, borough:1, cuisine:1})
+db.Restaurants.find({}, {restaurant_id:1, name:1, borough:1, cuisine:1, _id:0})
+db.Restaurants.find({}, {restaurant_id:1, name:1, borough:1, address:{zipcode:1}, _id:0})
+db.Restaurants.find({borough:'Bronx'}, {})
+db.Restaurants.find({borough:'Bronx'}, {}).limit(5)
+db.Restaurants.find({borough:'Bronx'}, {}).skip(5).limit(5)
+db.Restaurants.find({"grades.score":{$gt:90}})
