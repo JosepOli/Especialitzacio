c7d9d1091f3cbaa54872d73b4214fb506102060d
JosepOli <josepolive89@gmail.com>
2023-02-22 12:25:16 +0100

    Up to 22

--- Sprint2/mongoDB-queries/Restaurants.js
+++ Sprint2/mongoDB-queries/Restaurants.js
@@ -8,10 +8,13 @@ db.Restaurants.find({borough:'Bronx'}, {}).skip(5).limit(5)
 db.Restaurants.find({"grades.score":{$gt:90}})
 db.Restaurants.find({"grades.score":{$gt:80, $lt:100}})
 db.Restaurants.find({"address.coord":{$lt:-95.754168}})
-db.Restaurants.find({$and:[{ cuisine: { $ne: "American " } }, {"grades.score":{$gt:70}}, {"address.coord":{$lt:-65.754168}}]})
-db.Restaurants.find({cuisine: { $ne: "American " } , "grades.score":{$gt:70}, "address.coord":{$lt:-65.754168}})
+db.Restaurants.find({$and:[{cuisine: { $ne: "American " } }, {"grades.score":{$gt:70}}, {"address.coord":{$lt:-65.754168}}]})
+db.Restaurants.find({cuisine: {$ne: "American " } , "grades.score":{$gt:70}, "address.coord":{$lt:-65.754168}})
 db.Restaurants.find({$and:[{cuisine: { $ne: "American " } }, {"grades.grade": 'A'}, {borough:{ $ne: 'Brooklyn'}}]}).sort("cuisine")
-db.Restaurants.find({"name": { $regex: /^Wil/}}, {"restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1})
-db.Restaurants.find({"name": { $regex: /ces$/}}, {"restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1})
-
+db.Restaurants.find({"name": {$regex: /^Wil/}}, {"restaurant_id":1, "name":1, "borough":1, "cuisine":1})
+db.Restaurants.find({"name": {$regex: /ces$/}}, {"restaurant_id":1, "name":1, "borough":1, "cuisine":1})
+db.Restaurants.find({"name": {$regex: /Reg/}}, {"restaurant_id":1, "name":1, "borough":1, "cuisine":1})
+db.Restaurants.find({"borough": "Bronx", "cuisine": {$in: ["American", "Chinese"]}})
+db.restaurants.find({"borough": {$in: ["Staten Island", "Queens", "Bronx", "Brooklyn"]}}, {"restaurant_id":1, "name":1, "borough":1, "cuisine":1}
+ )
  
