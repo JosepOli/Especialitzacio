3ebd26ce30a3b9dcd81e5cd3fb5f571f8bc3cd52
JosepOli <josepolive89@gmail.com>
2023-03-21 19:13:44 +0100

    Classe Contacte

---
+++ Sprint 3/TascaS301N2/src/S301N2/Contacte.java
@@ -0,0 +1,33 @@
+package S301N2;
+
+public class Contacte {
+
+	private String nom;
+	private String cognoms;
+	private Direccio direccio;
+	private NumeroTelefon numeroTelefon;
+
+	public Contacte(String nom, String cognoms, Direccio direccio, NumeroTelefon numeroTelefon) {
+		this.nom = nom;
+		this.cognoms = cognoms;
+		this.direccio = direccio;
+		this.numeroTelefon = numeroTelefon;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getCognoms() {
+		return cognoms;
+	}
+
+	public Direccio getDireccio() {
+		return direccio;
+	}
+
+	public NumeroTelefon getNumeroTelefon() {
+		return numeroTelefon;
+	}
+
+}
