1f310e62b672e2ef03273224cf80c276b569a089
JosepOli <josepolive89@gmail.com>
2023-03-21 11:30:13 +0100

    Classe Direccio completa

--- Sprint 3/TascaS301N2/src/S301N2/Direccio.java
+++ Sprint 3/TascaS301N2/src/S301N2/Direccio.java
@@ -8,4 +8,38 @@ public abstract class Direccio {
 	private String provincia;
 	private String país;
 
+	 // Getters i setters
+    public String getCarrer() {
+        return carrer;
+    }
+    public void setCarrer(String carrer) {
+        this.carrer = carrer;
+    }
+    public String getCiutat() {
+        return ciutat;
+    }
+    public void setCiutat(String ciutat) {
+        this.ciutat = ciutat;
+    }
+    public String getCodiPostal() {
+        return codiPostal;
+    }
+    public void setCodiPostal(String codiPostal) {
+        this.codiPostal = codiPostal;
+    }
+    public String getProvincia() {
+        return provincia;
+    }
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+    public String getPaís() {
+        return país;
+    }
+    public void setPaís(String país) {
+        this.país = país;
+    }
+
+    // Mètode abstracte per imprimir la informació de la direcció
+    public abstract void imprimirDireccio();
 }
