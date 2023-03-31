65d75a46a97dfc411f9c82ad63b7592083cbb058
JosepOli <josepolive89@gmail.com>
2023-03-31 13:11:32 +0200

    Afegit gestió d'error d'entrada d'usuari

--- Sprint 3/TascaS301N2/src/s301N2/s301n2.java
+++ Sprint 3/TascaS301N2/src/s301N2/s301n2.java
@@ -15,8 +15,18 @@ public class s301n2 {
 		System.out.println("Introdueix els cognoms:");
 		String cognoms = scanner.nextLine();
 
-		System.out.println("Introdueix el país de la direcció:");
-		String pais = scanner.nextLine();
+		System.out.println("Introdueix el país de la direcció (espanya o franca):");
+
+		// Implementem gestió d'errors
+		String pais;
+		while (true) {
+			pais = scanner.nextLine().toLowerCase();
+			if (pais.equals("espanya") || pais.equals("franca")) {
+				break;
+			} else {
+				System.out.println("Si us plau, introdueix un país vàlid (espanya o franca):");
+			}
+		}
 
 		System.out.println("Introdueix el carrer:");
 		String carrer = scanner.nextLine();
@@ -31,15 +41,17 @@ public class s301n2 {
 		String provincia = scanner.nextLine();
 
 		System.out.println("Introdueix el prefix del número de telèfon:");
-		 String prefix;
-	        while (true) {
-	            prefix = scanner.nextLine();
-	            if (prefix.matches("^\\+\\d{1,3}$")) {
-	                break;
-	            } else {
-	                System.out.println("Si us plau, introdueix un prefix de telèfon vàlid (per exemple: +34):");
-	            }
-	        }
+
+		// Implementem gestió d'errors
+		String prefix;
+		while (true) {
+			prefix = scanner.nextLine();
+			if (prefix.matches("^\\+\\d{1,3}$")) {
+				break;
+			} else {
+				System.out.println("Si us plau, introdueix un prefix de telèfon vàlid (per exemple: +34):");
+			}
+		}
 
 		System.out.println("Introdueix el número de telèfon:");
 		String numero = scanner.nextLine();
