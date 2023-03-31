965135f0e5d5debd4f426b8ba7137b872870c8d8
JosepOli <josepolive89@gmail.com>
2023-03-31 13:01:00 +0200

    Modifiquem ContacteFactory per a instanciar paisos pels que hem creat classes

--- Sprint 3/TascaS301N2/src/s301N2/ContacteFactory.java
+++ Sprint 3/TascaS301N2/src/s301N2/ContacteFactory.java
@@ -2,11 +2,28 @@ package s301N2;
 
 public class ContacteFactory extends AbstractContacteFactory {
 
-    public Direccio crearDireccio(String carrer, String ciutat, String codiPostal, String provincia, String pais) {
-		return null;
-	}
 
+    @Override
+    public Direccio crearDireccio(String carrer, String ciutat, String codiPostal, String provincia, String pais) {
+        switch (pais) {
+            case "Espanya":
+                return new DireccioEspanya(carrer, ciutat, codiPostal, provincia, pais);
+            case "França":
+                return new DireccioFranca(carrer, ciutat, codiPostal, provincia, pais);
+            default:
+                throw new IllegalArgumentException("País no suportat");
+        }
+    }
+
+    @Override
     public NumeroTelefon crearNumeroTelefon(String prefix, String numero) {
-		return null;
-	}
+        switch (prefix) {
+            case "+34":
+                return new NumeroTelefonEspanya(prefix, numero);
+            case "+33":
+                return new NumeroTelefonFranca(prefix, numero);
+            default:
+                throw new IllegalArgumentException("Prefix no suportat");
+        }
+    }
 }
