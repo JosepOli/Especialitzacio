cf291c5c62b1fca6aa7af3fb132661bbeedfc267
JosepOli <josepolive89@gmail.com>
2023-04-01 11:41:28 +0200

    Primera part

--- Sprint 3/TascaS302N2/S302N2/lib/src/main/java/s302N2/BotigaSabates.java
+++ Sprint 3/TascaS302N2/S302N2/lib/src/main/java/s302N2/BotigaSabates.java
@@ -5,6 +5,17 @@ public class BotigaSabates {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		  PassarellaPagament passarellaPagament = new PassarellaPagament();
+	        double importPagament = 100.0;
+
+	        //Implementem callback emprant lambda
+	        Callback callback = resultat -> {
+	            if (resultat) {
+	                System.out.println("El pagament s'ha realitzat correctament.");
+	            } else {
+	                System.out.println("El pagament no s'ha pogut realitzar.");
+	            }
+	        };
 	}
 
 }
