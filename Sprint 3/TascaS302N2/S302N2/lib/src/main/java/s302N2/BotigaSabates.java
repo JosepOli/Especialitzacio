e5c034c43f66fd026121aede824e3b91be22e5e7
JosepOli <josepolive89@gmail.com>
2023-04-04 17:01:57 +0200

    UPD

--- Sprint 3/TascaS302N2/S302N2/lib/src/main/java/s302N2/BotigaSabates.java
+++ Sprint 3/TascaS302N2/S302N2/lib/src/main/java/s302N2/BotigaSabates.java
@@ -5,17 +5,25 @@ public class BotigaSabates {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		  PassarellaPagament passarellaPagament = new PassarellaPagament();
-	        double importPagament = 100.0;
+		PassarellaPagament passarellaPagament = new PassarellaPagament();
+		double importPagament = 100.0;
 
-	        //Implementem callback emprant lambda
-	        Callback callback = resultat -> {
-	            if (resultat) {
-	                System.out.println("El pagament s'ha realitzat correctament.");
-	            } else {
-	                System.out.println("El pagament no s'ha pogut realitzar.");
-	            }
-	        };
+		// Implementem callback emprant lambda
+		Callback callback = resultat -> {
+			if (resultat) {
+				System.out.println("El pagament s'ha realitzat correctament.");
+			} else {
+				System.out.println("El pagament no s'ha pogut realitzar.");
+			}
+
+		};
+
+		// Cridem la passarel·la de pagament per invocar el metode de pagament i
+		// processar el resultat a traves de la interficie "Callback"
+		passarellaPagament.processarPagament(new TargetaCredit(), importPagament, callback);
+		passarellaPagament.processarPagament(new Paypal(), importPagament, callback);
+		passarellaPagament.processarPagament(new DeuteCompteBancari(), importPagament, callback);
+
 	}
 
 }
