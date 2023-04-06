6c4784f35ed15ec30952e253f6c43981cfeb6c38
JosepOli <josepolive89@gmail.com>
2023-04-06 17:55:57 +0200

    Limitem els tipus de material fent un enum

--- Sprint 3/TascaS303N1/src/s303n1/Decoracio.java
+++ Sprint 3/TascaS303N1/src/s303n1/Decoracio.java
@@ -1,20 +1,25 @@
 package s303n1;
 
 public class Decoracio {
+	
+	public enum TipusDeMaterial{
+		fusta,
+		plastic
+	}
 
-	private String material;
+	private TipusDeMaterial material;
 	private double preu;
 
-	public Decoracio(String material, double preu) {
+	public Decoracio(TipusDeMaterial material, double preu) {
 		this.material = material;
 		this.preu = preu;
 	}
 
-	public String getMaterial() {
+	public TipusDeMaterial getMaterial() {
 		return material;
 	}
 
-	public void setMaterial(String material) {
+	public void setMaterial(TipusDeMaterial material) {
 		this.material = material;
 	}
 
