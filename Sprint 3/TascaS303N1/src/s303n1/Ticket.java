5835a5ea37945c97f136fe20d43f02c299e4d336
JosepOli <josepolive89@gmail.com>
2023-04-17 15:40:15 +0200

    Format corregit, afegim getters i  tostring

--- Sprint 3/TascaS303N1/src/s303n1/Ticket.java
+++ Sprint 3/TascaS303N1/src/s303n1/Ticket.java
@@ -7,18 +7,31 @@ public class Ticket {
 
 	private List<Object> elementsComprats;
 	private double importTotal;
-	
-	//Constructor
+
+	// Constructor
 	public Ticket() {
 		elementsComprats = new ArrayList<>();
 		importTotal = 0;
 	}
-	//Afegim element a la llista i sumem el preu a l'import total
-	public void afegirElement (Object element, double preu) {
+
+	// Afegim element a la llista i sumem el preu a l'import total
+	public void afegirElement(Object element, double preu) {
 		elementsComprats.add(element);
 		importTotal += preu;
 	}
-	
-	
-	
+
+	// Getters d'elements i import total
+	public List<Object> getElementsComprats() {
+		return elementsComprats;
+	}
+
+	public double getImportTotal() {
+		return importTotal;
+	}
+
+	@Override
+	public String toString() {
+		return "Ticket [elementsComprats=" + elementsComprats + ", importTotal=" + importTotal + "]";
+	}
+
 }
