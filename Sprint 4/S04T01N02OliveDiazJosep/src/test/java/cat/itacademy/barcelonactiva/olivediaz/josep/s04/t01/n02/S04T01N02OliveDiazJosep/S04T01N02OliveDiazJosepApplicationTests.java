c4eb3cc1311ca279328a2c5e6f9c716062714283
JosepOli <josepolive89@gmail.com>
2023-06-08 18:27:17 +0200

    First commit

---
+++ Sprint 4/S04T01N02OliveDiazJosep/src/test/java/cat/itacademy/barcelonactiva/olivediaz/josep/s04/t01/n02/S04T01N02OliveDiazJosep/S04T01N02OliveDiazJosepApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.olivediaz.josep.s04.t01.n02.S04T01N02OliveDiazJosep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N02OliveDiazJosepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
