ff88741bd4946bd4d1825d490311d4bd5f376c56
JosepOli <josepolive89@gmail.com>
2023-06-13 17:47:20 +0200

    First Commit S04T02N01

---
+++ Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/S04T02N01OliveDiazJosepApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01OliveDiazJosepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01OliveDiazJosepApplication.class, args);
+	}
+
+}
---
+++ Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/test/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/S04T02N01OliveDiazJosepApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N01OliveDiazJosepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
