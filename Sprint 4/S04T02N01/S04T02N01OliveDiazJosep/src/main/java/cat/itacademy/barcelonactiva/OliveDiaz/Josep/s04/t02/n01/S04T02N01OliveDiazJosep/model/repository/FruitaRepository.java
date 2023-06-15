abda4b6c543bf00435548a078f3d6e5f1cdbbd61
JosepOli <josepolive89@gmail.com>
2023-06-15 16:59:50 +0200

    Fruitarepository done

--- Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/model/repository/FruitaRepository.java
+++ Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/model/repository/FruitaRepository.java
@@ -1,5 +1,8 @@
 package cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.repository;
 
-public class FruitaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.domain.Fruita;
+
+public interface FruitaRepository extends JpaRepository<Fruita, Integer>{
 
 }
