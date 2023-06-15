8d87a2c7fa69ca3d39236a3fbeb91b0dd84c5c29
JosepOli <josepolive89@gmail.com>
2023-06-15 20:24:48 +0200

    UPD

--- Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/S04T02N01OliveDiazJosepApplication.java
+++ Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/S04T02N01OliveDiazJosepApplication.java
@@ -2,14 +2,8 @@ package cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveD
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {
-		"cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.domain",
-		"cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.repository" })
-@EntityScan("cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.domain")
 
 public class S04T02N01OliveDiazJosepApplication {
 
