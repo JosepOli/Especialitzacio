0f7d54982b598405879af07f09ec429b0385878c
JosepOli <josepolive89@gmail.com>
2023-06-15 18:45:49 +0200

    Methods created in the Controller. Now let's get into error handling.

--- Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/controllers/FruitaController.java
+++ Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/controllers/FruitaController.java
@@ -1,5 +1,42 @@
 package cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.controllers;
 
+import org.springframework.web.bind.annotation.*;
+import cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.services.FruitaService;
+import cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.domain.Fruita;
+import java.util.List;
+
+@RestController
+@RequestMapping("/fruita")
 public class FruitaController {
 
+	private final FruitaService fruitaService;
+
+	public FruitaController(FruitaService fruitaService) {
+		this.fruitaService = fruitaService;
+	}
+
+	@PostMapping("/add")
+	public Fruita addFruita(@RequestBody Fruita fruita) {
+		return fruitaService.addFruita(fruita);
+	}
+
+	@PutMapping("/update/{id}")
+	public Fruita updateFruita(@RequestBody Fruita fruita, @PathVariable int id) {
+		return fruitaService.update(fruita, id);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void deleteFruita(@PathVariable int id) {
+		fruitaService.delete(id);
+	}
+
+	@GetMapping("/getOne/{id}")
+	public Fruita getOneFruita(@PathVariable int id) {
+		return fruitaService.getOneFruita(id);
+	}
+
+	@GetMapping("/getAll")
+	public List<Fruita> getAllFruita() {
+		return fruitaService.getAllFruita();
+	}
 }
