ae6dc2e43203d5c41025898cbadb906fbdfa3eb7
JosepOli <josepolive89@gmail.com>
2023-06-15 18:17:14 +0200

    ADD, update, delete and getAll methods included

--- Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/model/services/FruitaService.java
+++ Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/model/services/FruitaService.java
@@ -1,5 +1,35 @@
 package cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.services;
 
+import org.springframework.stereotype.Service;
+import java.util.List;
+import cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.repository.*;
+import cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.domain.*;
+
+@Service
 public class FruitaService {
+	private final FruitaRepository fruitaRepository;
+
+	public FruitaService(FruitaRepository fruitaRepository) {
+		this.fruitaRepository = fruitaRepository;
+	}
+
+	public Fruita addFruita(Fruita fruita) { // Afegim una fruita
+		return fruitaRepository.save(fruita);
+	}
+
+	public Fruita update(Fruita updatedFruita, int id) { // Actualitzem fruita
+		Fruita existingFruita = fruitaRepository.getReferenceById(id);
+		existingFruita.setNom(updatedFruita.getNom());
+		existingFruita.setQuantitatQuilos(updatedFruita.getQuantitatQuilos());
+		return fruitaRepository.save(existingFruita);
+	}
+
+	public void delete(int id) { // Eliminem fruita amb id
+		fruitaRepository.deleteById(id);
+	}
+
+	public List<Fruita> getAllFruita() { // Retornem totes les fruites
+		return fruitaRepository.findAll();
+	}
 
 }
