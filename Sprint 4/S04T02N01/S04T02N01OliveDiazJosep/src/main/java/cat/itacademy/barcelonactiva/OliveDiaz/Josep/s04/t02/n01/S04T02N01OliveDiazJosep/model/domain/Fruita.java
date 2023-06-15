c94c56f44a0ef5e7be5021869b21e6dfbc7557dd
JosepOli <josepolive89@gmail.com>
2023-06-15 16:52:04 +0200

    Fruita creada i dependency javax.persistence included

--- Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/model/domain/Fruita.java
+++ Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosep/model/domain/Fruita.java
@@ -1,5 +1,35 @@
 package cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep.model.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Fruita {
 
+	@Id
+	@GeneratedValue
+	private int id;
+	private String nom;
+	private int quantitatQuilos;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getQuantitatQuilos() {
+		return quantitatQuilos;
+	}
+	public void setQuantitatQuilos(int quantitatQuilos) {
+		this.quantitatQuilos = quantitatQuilos;
+	}
+	
 }
