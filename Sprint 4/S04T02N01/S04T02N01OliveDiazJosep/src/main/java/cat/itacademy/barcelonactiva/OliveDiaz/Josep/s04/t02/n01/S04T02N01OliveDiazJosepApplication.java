0f9a4a044a1086cb202746cbe424282f2c98de9d
JosepOli <josepolive89@gmail.com>
2023-06-19 22:21:28 +0200

    UPD Fix

--- Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosepApplication.java
+++ Sprint 4/S04T02N01/S04T02N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/OliveDiaz/Josep/s04/t02/n01/S04T02N01OliveDiazJosepApplication.java
@@ -1,4 +1,4 @@
-package cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01.S04T02N01OliveDiazJosep;
+package cat.itacademy.barcelonactiva.OliveDiaz.Josep.s04.t02.n01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
