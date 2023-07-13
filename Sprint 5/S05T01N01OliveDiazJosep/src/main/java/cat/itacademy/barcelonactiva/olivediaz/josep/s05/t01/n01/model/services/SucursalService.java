d2daf6836ec957120406c6afe99a12fc3e14466d
JosepOli <109340762+JosepOli@users.noreply.github.com>
2023-07-13 18:06:46 +0200

    UPD sucursalservice

--- Sprint 5/S05T01N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n01/model/services/SucursalService.java
+++ Sprint 5/S05T01N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n01/model/services/SucursalService.java
@@ -1,5 +1,37 @@
 package cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n01.model.services;
 
+import cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n01.model.domain.Sucursal;
+import cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n01.model.repository.SucursalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class SucursalService {
 
+	@Autowired
+	private SucursalRepository sucursalRepository;
+
+	public Sucursal addSucursal(Sucursal sucursal) {
+		return sucursalRepository.save(sucursal);
+	}
+
+	public Sucursal updateSucursal(Sucursal sucursal) {
+		return sucursalRepository.save(sucursal);
+	}
+
+	public void deleteSucursal(Integer id) {
+		sucursalRepository.deleteById(id);
+	}
+
+	public Sucursal getSucursal(Integer id) {
+		Optional<Sucursal> optionalSucursal = sucursalRepository.findById(id);
+		return optionalSucursal.orElse(null);
+	}
+
+	public List<Sucursal> getAllSucursals() {
+		return sucursalRepository.findAll();
+	}
 }
