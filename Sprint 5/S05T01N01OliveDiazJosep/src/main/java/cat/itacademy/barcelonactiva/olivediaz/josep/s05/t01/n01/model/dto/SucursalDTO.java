09fb977a19551da34c0ae7eb2c557252b6dc8250
JosepOli <109340762+JosepOli@users.noreply.github.com>
2023-07-13 17:50:50 +0200

    SucursalDTO ok

--- Sprint 5/S05T01N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n01/model/dto/SucursalDTO.java
+++ Sprint 5/S05T01N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n01/model/dto/SucursalDTO.java
@@ -1,5 +1,58 @@
 package cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n01.model.dto;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SucursalDTO {
+
+	private Integer pk_SucursalID;
+	private String nomSucursal;
+	private String paisSucursal;
+	private String tipusSucursal;
+	
+	private List<String> paisos = Arrays.asList("Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech Republic",
+			"Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia",
+			"Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia",
+			"Spain", "Sweden");
+
+	public String getTipusSucursal() {
+		return paisos.contains(paisSucursal) ? "UE" : "Fora UE";
+	}
+
+	public Integer getPk_SucursalID() {
+		return pk_SucursalID;
+	}
+
+	public void setPk_SucursalID(Integer pk_SucursalID) {
+		this.pk_SucursalID = pk_SucursalID;
+	}
+
+	public String getNomSucursal() {
+		return nomSucursal;
+	}
+
+	public void setNomSucursal(String nomSucursal) {
+		this.nomSucursal = nomSucursal;
+	}
+
+	public String getPaisSucursal() {
+		return paisSucursal;
+	}
+
+	public void setPaisSucursal(String paisSucursal) {
+		this.paisSucursal = paisSucursal;
+	}
+
+	public List<String> getPaïsos() {
+		return paisos;
+	}
+
+	public void setPaïsos(List<String> països) {
+		this.paisos = països;
+	}
+
+	public void setTipusSucursal(String tipusSucursal) {
+		this.tipusSucursal = tipusSucursal;
+	}
 
 }
