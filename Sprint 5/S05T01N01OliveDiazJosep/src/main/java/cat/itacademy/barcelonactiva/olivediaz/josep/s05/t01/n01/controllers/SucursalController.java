272179d4dbe2a8e6cb4b52b030773cbfe9d12ee0
JosepOli <josepolive89@gmail.com>
2023-07-18 12:18:28 +0200

    Working version

--- Sprint 5/S05T01N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n01/controllers/SucursalController.java
+++ Sprint 5/S05T01N01OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n01/controllers/SucursalController.java
@@ -3,11 +3,13 @@ package cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n01.controllers;
 import cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n01.model.domain.Sucursal;
 import cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n01.model.services.SucursalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/sucursal")
 public class SucursalController {
 
@@ -15,28 +17,34 @@ public class SucursalController {
     private SucursalService sucursalService;
 
     @PostMapping("/add")
-    public Sucursal addSucursal(@RequestBody Sucursal sucursal) {
+    public @ResponseBody Sucursal addSucursal(@RequestBody Sucursal sucursal) {
         return sucursalService.addSucursal(sucursal);
     }
 
     @PutMapping("/update")
-    public Sucursal updateSucursal(@RequestBody Sucursal sucursal) {
+    public @ResponseBody Sucursal updateSucursal(@RequestBody Sucursal sucursal) {
         return sucursalService.updateSucursal(sucursal);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteSucursal(@PathVariable Integer id) {
+    public @ResponseBody void deleteSucursal(@PathVariable Integer id) {
         sucursalService.deleteSucursal(id);
     }
 
     @GetMapping("/getOne/{id}")
-    public Sucursal getSucursal(@PathVariable Integer id) {
+    public @ResponseBody Sucursal getSucursal(@PathVariable Integer id) {
         return sucursalService.getSucursal(id);
     }
 
     @GetMapping("/getAll")
-    public List<Sucursal> getAllSucursals() {
+    public @ResponseBody List<Sucursal> getAllSucursals() {
         return sucursalService.getAllSucursals();
     }
-}
 
+    @GetMapping("/all")
+    public String showAllSucursals(Model model) {
+        List<Sucursal> sucursals = sucursalService.getAllSucursals();
+        model.addAttribute("sucursals", sucursals);
+        return "allSucursals";
+    }
+}
