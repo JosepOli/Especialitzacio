24ec5d43baaeedaa8b90edd5a8e54e3356bfad07
JosepOli <josepolive89@gmail.com>
2023-07-18 15:49:12 +0200

    Upd name + flor entity

--- S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/S05T01N02OliveDiazJosepApplication.java
+++ S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/S05T01N02OliveDiazJosepApplication.java
@@ -1,4 +1,4 @@
-package cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.S05T01N02OliveDiazJosep;
+package cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
