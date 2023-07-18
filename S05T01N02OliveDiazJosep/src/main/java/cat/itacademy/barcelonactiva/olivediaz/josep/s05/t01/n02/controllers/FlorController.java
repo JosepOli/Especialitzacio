16b4c493140f7cc1e12f9f6631a82b5f52a37362
JosepOli <josepolive89@gmail.com>
2023-07-18 17:27:02 +0200

    Controller add

--- S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/controllers/FlorController.java
+++ S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/controllers/FlorController.java
@@ -1,5 +1,23 @@
 package cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.model.dto.FlorDTO;
+import cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.model.services.FlorService;
+
+@RestController
+@RequestMapping("/flor")
 public class FlorController {
 
+	 @Autowired
+	    private FlorService florService;
+
+	    @PostMapping("/add")
+	    public ResponseEntity<FlorDTO> addFlor(@RequestBody FlorDTO florDTO) {
+	        FlorDTO result = florService.addFlor(florDTO);
+	        return new ResponseEntity<>(result, HttpStatus.CREATED);
+	    }
 }
