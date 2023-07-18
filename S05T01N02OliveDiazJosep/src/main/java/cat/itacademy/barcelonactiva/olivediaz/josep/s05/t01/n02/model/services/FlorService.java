089f2aca19d5462b79e523fd6ae6513d79a3f729
JosepOli <josepolive89@gmail.com>
2023-07-18 17:24:07 +0200

    FlorService complete

--- S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/model/services/FlorService.java
+++ S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/model/services/FlorService.java
@@ -1,5 +1,9 @@
 package cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.model.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,23 +15,49 @@ import cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.model.repository
 public class FlorService {
 
 	@Autowired
-    private FlorRepository florRepository;
+	private FlorRepository florRepository;
 
-    // Convert entity to DTO
-    private FlorDTO convertToDTO(FlorEntity entity) {
-        FlorDTO dto = new FlorDTO();
-        dto.setPk_FlorID(entity.getPk_FlorID());
-        dto.setNomFlor(entity.getNomFlor());
-        dto.setPaisFlor(entity.getPaisFlor());
-        return dto;
-    }
+	// Convert entity to DTO
+	private FlorDTO convertToDTO(FlorEntity entity) {
+		FlorDTO dto = new FlorDTO();
+		dto.setPk_FlorID(entity.getPk_FlorID());
+		dto.setNomFlor(entity.getNomFlor());
+		dto.setPaisFlor(entity.getPaisFlor());
+		return dto;
+	}
 
-    // Convert DTO to entity
-    private FlorEntity convertToEntity(FlorDTO dto) {
-        FlorEntity entity = new FlorEntity();
-        entity.setPk_FlorID(dto.getPk_FlorID());
-        entity.setNomFlor(dto.getNomFlor());
-        entity.setPaisFlor(dto.getPaisFlor());
-        return entity;
-    }
+	// Convert DTO to entity
+	private FlorEntity convertToEntity(FlorDTO dto) {
+		FlorEntity entity = new FlorEntity();
+		entity.setPk_FlorID(dto.getPk_FlorID());
+		entity.setNomFlor(dto.getNomFlor());
+		entity.setPaisFlor(dto.getPaisFlor());
+		return entity;
+	}
+
+	public FlorDTO addFlor(FlorDTO florDTO) {
+		FlorEntity entity = convertToEntity(florDTO);
+		entity = florRepository.save(entity);
+		return convertToDTO(entity);
+	}
+
+	public FlorDTO updateFlor(FlorDTO florDTO) {
+		FlorEntity entity = convertToEntity(florDTO);
+		entity = florRepository.save(entity);
+		return convertToDTO(entity);
+	}
+
+	public void deleteFlor(Integer id) {
+		florRepository.deleteById(id);
+	}
+
+	public FlorDTO getOneFlor(Integer id) {
+		Optional<FlorEntity> entity = florRepository.findById(id);
+		return entity.map(this::convertToDTO).orElse(null);
+	}
+
+	public List<FlorDTO> getAllFlor() {
+		List<FlorEntity> entities = florRepository.findAll();
+		return entities.stream().map(this::convertToDTO).collect(Collectors.toList());
+	}
 }
