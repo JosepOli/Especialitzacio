e30808b0b6c69d1d17b6efb4070554e3d6f3c078
JosepOli <josepolive89@gmail.com>
2023-07-18 17:28:15 +0200

    Controller getall

--- S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/controllers/FlorController.java
+++ S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/controllers/FlorController.java
@@ -1,5 +1,7 @@
 package cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,6 +40,12 @@ public class FlorController {
 		FlorDTO result = florService.getOneFlor(id);
 		if (result == null)
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	@GetMapping("/getAll")
+	public ResponseEntity<List<FlorDTO>> getAllFlor() {
+		List<FlorDTO> result = florService.getAllFlor();
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
