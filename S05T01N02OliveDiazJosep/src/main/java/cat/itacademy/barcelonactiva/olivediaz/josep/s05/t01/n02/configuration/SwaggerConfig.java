c57d55b32bbcfe3872ed7ee6c34ec9ea2358b9c3
JosepOli <josepolive89@gmail.com>
2023-07-18 20:48:24 +0200

    Updated as a workaround due to swagger being outdated

--- S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/configuration/SwaggerConfig.java
+++ S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/configuration/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.configuration;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,12 +13,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors
-						.basePackage("cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.controllers"))
-				.paths(PathSelectors.any()).build();
-	}
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
