32528b2f6663ca323a787d474874a7553bff8c81
JosepOli <josepolive89@gmail.com>
2023-07-25 12:53:11 +0200

    Upd with spaces

--- Sprint 5/S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/model/services/FlorServiceInterface.java
+++ Sprint 5/S05T01N02OliveDiazJosep/src/main/java/cat/itacademy/barcelonactiva/olivediaz/josep/s05/t01/n02/model/services/FlorServiceInterface.java
@@ -4,9 +4,13 @@ import java.util.List;
 import cat.itacademy.barcelonactiva.olivediaz.josep.s05.t01.n02.model.dto.FlorDTO;
 
 public interface FlorServiceInterface {
-    FlorDTO addFlor(FlorDTO florDTO);
-    FlorDTO updateFlor(FlorDTO florDTO);
-    void deleteFlor(Integer id);
-    FlorDTO getOneFlor(Integer id);
-    List<FlorDTO> getAllFlor();
+	FlorDTO addFlor(FlorDTO florDTO);
+
+	FlorDTO updateFlor(FlorDTO florDTO);
+
+	void deleteFlor(Integer id);
+
+	FlorDTO getOneFlor(Integer id);
+
+	List<FlorDTO> getAllFlor();
 }
