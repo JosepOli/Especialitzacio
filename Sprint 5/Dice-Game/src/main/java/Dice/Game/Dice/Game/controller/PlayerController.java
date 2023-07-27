37f0f127d1b4777aaca25e36f116dbceea66fb00
JosepOli <109340762+JosepOli@users.noreply.github.com>
2023-07-27 15:47:39 +0200

    PlayerController done

---
+++ Sprint 5/Dice-Game/src/main/java/Dice/Game/Dice/Game/controller/PlayerController.java
@@ -0,0 +1,48 @@
+package Dice.Game.Dice.Game.controller;
+
+import Dice.Game.Dice.Game.model.dto.PlayerDTO;
+import Dice.Game.Dice.Game.model.services.PlayerServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/players")
+public class PlayerController {
+
+	@Autowired
+	private PlayerServiceInterface playerService;
+
+	@PostMapping
+	public ResponseEntity<PlayerDTO> createPlayer(@RequestBody PlayerDTO playerDTO) {
+		PlayerDTO createdPlayer = playerService.createPlayer(playerDTO);
+		return new ResponseEntity<>(createdPlayer, HttpStatus.CREATED);
+	}
+
+	@PutMapping
+	public ResponseEntity<PlayerDTO> updatePlayer(@RequestBody PlayerDTO playerDTO) {
+		PlayerDTO updatedPlayer = playerService.updatePlayer(playerDTO);
+		return new ResponseEntity<>(updatedPlayer, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}/games")
+	public ResponseEntity<?> deleteGamesByPlayerId(@PathVariable Long id) {
+		playerService.deleteGamesByPlayerId(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<PlayerDTO>> getAllPlayers() {
+		List<PlayerDTO> players = playerService.getAllPlayers();
+		return new ResponseEntity<>(players, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<PlayerDTO> getPlayerById(@PathVariable Long id) {
+		PlayerDTO player = playerService.getPlayerById(id);
+		return new ResponseEntity<>(player, HttpStatus.OK);
+	}
+}
