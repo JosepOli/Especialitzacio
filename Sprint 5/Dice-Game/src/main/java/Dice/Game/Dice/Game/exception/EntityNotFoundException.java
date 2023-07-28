aa5b8955030c91b636a2cff5882c3359e2772bed
JosepOli <109340762+JosepOli@users.noreply.github.com>
2023-07-28 09:43:04 +0200

    Exception created to handle entities not found

---
+++ Sprint 5/Dice-Game/src/main/java/Dice/Game/Dice/Game/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package Dice.Game.Dice.Game.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+	public EntityNotFoundException(String message) {
+		super(message);
+	}
+}
